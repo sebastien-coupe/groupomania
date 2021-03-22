c36d231fa3ba545024460f8073b62a4fd6edc0a8
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-03-22 16:10:21 +0100

    API: Added post controller logic

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,25 +1,68 @@
+const { Post } = require('../models');
+
 exports.findAll = async ctx => {
+  const posts = await Post.findAll();
+
   ctx.body = {
     status: 'success',
-    message: 'Fetch all posts'
+    posts
   }
 }
 
 exports.findOne = async ctx => {
+  const { uuid } = ctx.params;
+
+  const post = await Post.findOne({
+    where: {
+      uuid
+    }
+  });
+
+  if (!post) ctx.throw(404, 'No post matching uuid');
+
   ctx.body = {
     status: 'success',
-    message: `Fetch post identified by ${ctx.params.uuid} uuid`
+    post
   }
 }
 
 exports.create = async ctx => {
+  const { body, imageUrl, userId } = ctx.request.body;
+
+  if (!body || !userId) ctx.throw(400, 'Request is not valid')
+
+  const createdPost = await Post.create({
+    body,
+    imageUrl,
+    userId
+  });
+
+  if (!createdPost) ctx.throw(500, 'Database error saving new post')
+
+  ctx.status = 201;
   ctx.body = {
     status: 'success',
-    message: 'Create a new post'
+    message: 'New post has been created successfully'
   }
 }
 
 exports.update = async ctx => {
+  const body = ctx.request.body;
+  const { uuid } = ctx.params;
+
+  if (!Object.keys(body).length) ctx.throw(400, 'Request is not valid');
+
+  const [updatedPost] = await Post.update(
+    body,
+    {
+      where: {
+        uuid
+      }
+    }
+  );
+
+  if (!updatedPost) ctx.throw(400, 'No post matching uuid')
+
   ctx.body = {
     status: 'success',
     message: `Update post identified by ${ctx.params.uuid} uuid`
@@ -27,8 +70,18 @@ exports.update = async ctx => {
 }
 
 exports.delete = async ctx => {
+  const { uuid } = ctx.params;
+
+  const deletedPost = await Post.destroy({
+    where: {
+      uuid
+    }
+  });
+
+  if (!deletedPost) ctx.throw(400, 'No post matching uuid');
+
   ctx.body = {
     status: 'success',
-    message: `Delete post identified by ${ctx.params.uuid} uuid`
+    message: `Delete post identified by ${uuid} uuid`
   }
 }
