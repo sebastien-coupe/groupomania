b6ac964ede185c816b3b30af43006fac0646bf1b
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-03-31 16:18:11 +0200

    CLIENT: Updated navbar to hide link relative to the current page

--- client/src/components/TheNavbar.vue
+++ client/src/components/TheNavbar.vue
@@ -4,12 +4,14 @@
       <router-link to="/" class="navbar-brand">Groupomania</router-link>
 
       <ul class="navbar-nav align-items-center ml-auto mb-lg-0">
-        <li class="nav-item">
-          <router-link to="/" class="nav-link">Log In</router-link>
+        <li v-if="$route.path !== '/signin'" class="nav-item">
+          <router-link to="/signin" class="btn btn-primary btn"
+            >Connexion</router-link
+          >
         </li>
-        <li class="nav-item ms-2 ms-sm-4">
-          <router-link to="/" class="btn btn-primary btn-sm"
-            >Sign Up</router-link
+        <li v-if="$route.path !== '/signup'" class="nav-item ms-2 ms-sm-4">
+          <router-link to="/signup" class="btn btn-primary btn"
+            >Inscription</router-link
           >
         </li>
       </ul>
