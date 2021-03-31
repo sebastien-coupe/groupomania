e88760333d375f719294ee0e8d7a1113345f74ae
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-03-31 16:02:36 +0200

    CLIENT: Updated app to always show navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column min-vh-100">
-    <Navbar v-if="isAuthenticated" />
+    <Navbar />
     <section class="d-flex flex-column flex-grow-1">
       <router-view />
     </section>
@@ -13,12 +13,6 @@ import Navbar from '@/components/TheNavbar';
 import Footer from '@/components/TheFooter';
 
 export default {
-  data() {
-    return {
-      // To refactor using vuex
-      isAuthenticated: false,
-    };
-  },
   components: {
     Navbar,
     Footer,
