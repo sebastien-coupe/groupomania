0bd7af59d53307d6f6f233967999c8e9b9cecbbb
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-04-01 14:48:38 +0200

    CLIENT: Added vuex as dependency

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 
-const app = createApp(App).use(router);
+const app = createApp(App).use(store).use(router);
 
 app.provide('API_URL', 'http://localhost:3000/api');
 
