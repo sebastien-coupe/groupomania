b8a6ee85fe813c06d406a389fd958bddd2866660
Sebastien Coupe <sebastien.coupe@protonmail.com>
2021-04-10 11:42:36 +0200

    CLIENT: Moved post submission form into a dedicated component

---
+++ client/src/components/PostForm.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="card">
+    <div class="d-flex align-items-center p-3">
+      <img
+        class="thumbnail-sm d-block"
+        src="https://randomuser.me/api/portraits/men/32.jpg"
+        alt=""
+      />
+      <div class="d-block ms-3 lh-sm">
+        <span class="fs-5 text-secondary">Quoi de neuf ?</span>
+      </div>
+      <div v-if="!showForm" class="ms-auto">
+        <button @click="toggleForm" class="btn btn-primary btn-sm">
+          <i class="bi bi-pencil"></i>
+        </button>
+      </div>
+    </div>
+    <div v-if="showForm" class="card-body">
+      <form @submit.prevent="submitForm">
+        <textarea v-model="body" class="form-control" rows="6"></textarea>
+        <div class="mt-2">
+          <button
+            @click.prevent="toggleImageInput"
+            class="btn btn-outline-secondary btn-sm"
+            title="Ajouter une image"
+          >
+            <i class="bi bi-card-image"></i>
+          </button>
+        </div>
+        <div class="mt-2">
+          <input
+            v-if="imageInput"
+            @change="preloadImage"
+            ref="image"
+            class="form-control form-control-sm"
+            type="file"
+            id="image"
+          />
+          <div v-if="imagePreview" class="mt-2">
+            <img :src="imagePreview" class="w-25 rounded" alt="preview" />
+          </div>
+        </div>
+        <div class="text-end mt-3">
+          <button @click="clearForm" class="btn btn-danger btn-sm">
+            Annuler
+          </button>
+          <button class="btn btn-success btn-sm ms-2">Publier</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostForm',
+
+  inject: ['API_URL'],
+
+  data() {
+    return {
+      body: '',
+      image: null,
+      imagePreview: '',
+      showForm: false,
+      imageInput: false,
+    };
+  },
+
+  methods: {
+    toggleForm() {
+      this.showForm = !this.showForm;
+    },
+
+    toggleImageInput() {
+      this.imageInput = !this.imageInput;
+    },
+
+    clearForm() {
+      this.body = '';
+      this.video = '';
+      this.imagePreview = '';
+      this.videoPreview = '';
+      this.imageInput = false;
+      this.toggleForm();
+    },
+
+    preloadImage() {
+      let input = this.$refs.image;
+      let file = input.files;
+
+      if (file && file[0]) {
+        this.image = file[0];
+
+        let reader = new FileReader();
+        reader.onload = (e) => {
+          this.imagePreview = e.target.result;
+        };
+        reader.readAsDataURL(file[0]);
+      }
+
+      console.log(this.image);
+    },
+
+    async submitForm() {
+      const data = new FormData();
+
+      data.append('body', this.body);
+      data.append('uid', this.$store.getters.userId);
+
+      if (this.image) {
+        data.append('image', this.image);
+      }
+      const headers = new Headers();
+
+      headers.append('Authorization', `Bearer ${this.$store.getters.token}`);
+
+      const response = await fetch(`${this.API_URL}/posts`, {
+        method: 'POST',
+        body: data,
+        headers,
+      });
+
+      const result = await response.json();
+
+      this.clearForm();
+    },
+  },
+};
+</script>
+
+<style>
+.thumbnail-sm {
+  width: 32px;
+  border-radius: 50%;
+}
+</style>
--- client/src/views/Feed.vue
+++ client/src/views/Feed.vue
@@ -2,49 +2,7 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col col-sm-10 col-md-8">
-        <div class="card">
-          <div class="d-flex align-items-center p-3">
-            <img
-              class="thumbnail-sm d-block"
-              src="https://randomuser.me/api/portraits/men/32.jpg"
-              alt=""
-            />
-            <div class="d-block ms-3 lh-sm">
-              <span class="fs-5 text-secondary">Quoi de neuf ?</span>
-            </div>
-            <div class="ms-auto">
-              <button class="btn btn-primary btn-sm">
-                <i class="bi bi-pencil"></i>
-              </button>
-            </div>
-          </div>
-          <div class="card-body">
-            <form action="">
-              <textarea class="form-control" rows="6"></textarea>
-              <div class="mt-2">
-                <label for="image" class="small">Ajouter une image ?</label>
-                <input
-                  class="form-control form-control-sm"
-                  type="file"
-                  id="image"
-                />
-              </div>
-              <div class="mt-2">
-                <label for="video" class="small">Ajouter une vidéo ?</label>
-                <input
-                  class="form-control form-control-sm"
-                  type="text"
-                  id="video"
-                  placeholder="ex: https://youtu.be/CxwJrzEdw1U"
-                />
-              </div>
-              <div class="text-end mt-3">
-                <button class="btn btn-danger btn-sm">Annuler</button>
-                <button class="btn btn-success btn-sm ms-2">Publier</button>
-              </div>
-            </form>
-          </div>
-        </div>
+        <PostForm />
         <Loader v-if="isLoading" />
         <Post v-else v-for="post in posts" :key="post.uuid" :post="post" />
       </div>
@@ -54,6 +12,7 @@
 
 <script>
 import Loader from '@/components/TheLoader';
+import PostForm from '@/components/PostForm';
 import Post from '@/components/PostCard';
 
 export default {
@@ -63,6 +22,7 @@ export default {
 
   components: {
     Loader,
+    PostForm,
     Post,
   },
 
@@ -96,10 +56,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.thumbnail-sm {
-  width: 32px;
-  border-radius: 50%;
-}
-</style>
